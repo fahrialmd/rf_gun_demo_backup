5e52c8f8a4eb4d431b84ae9c2f6451f42e989459
Iqbal <iqbal.nurakbar25@gmail.com>
2025-07-09 10:08:03 +0700

    retrieval data from backend side, need to filter in detail page

--- webapp/controller/DataDetail.controller.js
+++ webapp/controller/DataDetail.controller.js
@@ -3,23 +3,55 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  (Controller, MessageToast, History) => {
+  (Controller, MessageToast, History, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("rfgundemo.controller.DataDetail", {
       onInit() {
         const oRouter = this.getOwnerComponent().getRouter();
         const oModel = this.getOwnerComponent().getModel();
-        oRouter.attachRouteMatched(
-          function (oEvent) {
-            const oArgs = oEvent.getParameter("arguments");
-            if (oArgs && oArgs.purchaseOrderNumber) {
-              const sPurchaseOrder = oArgs.purchaseOrderNumber;
-            }
-            oModel.refresh();
-          }.bind(this)
+
+        const oPurchaseOrderModel = new JSONModel({
+          purchaseOrderNumber: "",
+        });
+
+        this.getView().setModel(oPurchaseOrderModel, "purchaseOrder");
+
+        oRouter.attachRouteMatched(function (oEvent) {
+          const oArgs = oEvent.getParameter("arguments");
+          if (oArgs && oArgs.purchaseOrderNumber) {
+            const sPurchaseOrder = oArgs.purchaseOrderNumber;
+            oPurchaseOrderModel.setProperty(
+              "/purchaseOrderNumber",
+              sPurchaseOrder
+            );
+          }
+          oModel.refresh();
+        });
+
+        const oList = this.byId("orderList");
+        console.log("Order List:", oList);
+
+        const oBinding = oList.getBinding("items");
+        const sPONumber = oPurchaseOrderModel.getProperty(
+          "/purchaseOrderNumber"
         );
+
+        if (oBinding) {
+          // Create a filter object
+          const oFilter = new Filter(
+            "PurchaseOrderNo",
+            FilterOperator.EQ,
+            sPONumber
+          );
+
+          // Apply the filter to the list binding
+          oBinding.filter([oFilter]);
+        }
       },
 
       onNavBack: function () {
@@ -31,6 +63,23 @@ sap.ui.define(
         } else {
           const oRouter = this.getOwnerComponent().getRouter();
           oRouter.navTo("RouteMainScreen", {}, true);
+        }
+      },
+
+      _applyPurchaseOrderFilter: function (sPurchaseOrder) {
+        const oList = this.byId("orderList");
+        const oBinding = oList.getBinding("items");
+
+        if (oBinding) {
+          // Create a filter object
+          const oFilter = new Filter(
+            "PurchaseOrderNo",
+            FilterOperator.EQ,
+            sPurchaseOrder
+          );
+
+          // Apply the filter to the list binding
+          oBinding.filter([oFilter]);
         }
       },
     });
--- webapp/controller/MainScreen.controller.js
+++ webapp/controller/MainScreen.controller.js
@@ -1,6 +1,11 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  (Controller, MessageToast) => {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+  ],
+  (Controller, MessageToast, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("rfgundemo.controller.MainScreen", {
@@ -23,57 +28,57 @@ sap.ui.define(
           oPurchaseOrderInput.setValueStateText(
             "Purchase Order number is required."
           );
+          oPurchaseOrderInput.focus();
           return;
-        } else {
-          oPurchaseOrderInput.setValueState("None");
-          oPurchaseOrderInput.setValueStateText("");
-
-          // TODO: Validate the input format
-          // Check if the input is valid from the OData Service
-
-          const oModel = this.getView().getModel();
-          const sPath = `/ZR_RF_PO_ITEM(PurchaseOrderNo='4500000001')`;
-          const oContext = oModel.bindContext(sPath);
-          console.log("Binding context:", oContext);
-          const oPurchaseOrderData = oContext.requestObject();
-          console.log("Purchase Order Data:", oPurchaseOrderData);
-          // const oBindingContext = oModel.bindList(
-          //   "/ZR_RF_PO_ITEM",
-          //   null,
-          //   null,
-          //   new sap.ui.model.Filter({
-          //     path: "PurchaseOrder",
-          //     operator: sap.ui.model.FilterOperator.EQ,
-          //     value1: sPurchaseOrder,
-          //   })
-          // );
-          // console.log("Binding context:", oBindingContext);
-          // oBindingContext
-          //   .requestContexts()
-          //   .then(
-          //     function (aContexts) {
-          //       // Check if any contexts are returned
-          //       if (aContexts.length > 0) {
-          //         // Purchase Order found
-          //         MessageToast.show(
-          //           `Purchase Order ${sPurchaseOrder} found. Navigating to detail.`
-          //         );
-          //         this._navigateToDetail(sPurchaseOrder);
-          //       } else {
-          //         // Purchase Order not found
-          //         MessageToast.show(
-          //           `Purchase Order ${sPurchaseOrder} not found. Please check the number.`
-          //         );
-          //       }
-          //     }.bind(this)
-          //   )
-          //   .catch(function (oError) {
-          //     console.error("Error loading", oError);
-          //     MessageToast.show("Error loading data. Please try again.");
-          //   });
-
-          this._navigateToDetail(sPurchaseOrder);
         }
+        // Reset value state if input is valid
+        oPurchaseOrderInput.setValueState("None");
+        oPurchaseOrderInput.setValueStateText("");
+
+        // TODO: Validate the input format
+        // Check if the input is valid from the OData Service
+
+        const oModel = this.getView().getModel();
+        const oBindingContext = oModel.bindList(
+          "/ZR_RF_PO_ITEM_MAIN",
+          null,
+          null,
+          new Filter({
+            path: "PurchaseOrderNo",
+            operator: FilterOperator.EQ,
+            value1: sPurchaseOrder,
+          })
+        );
+        console.log("Binding context:", oBindingContext);
+        oBindingContext
+          .requestContexts()
+          .then(
+            function (aContexts) {
+              // Check if any contexts are returned
+              if (aContexts.length > 0) {
+                // Purchase Order found
+                console.log("Purchase Order found:", aContexts[0].getObject());
+                MessageToast.show(
+                  `Purchase Order ${sPurchaseOrder} found. Navigating to detail.`
+                );
+                this._navigateToDetail(sPurchaseOrder);
+              } else {
+                // Purchase Order not found
+                MessageToast.show(
+                  `Purchase Order ${sPurchaseOrder} not found. Please check the number.`
+                );
+              }
+            }.bind(this)
+          )
+          .catch(
+            function (oError) {
+              console.error(oError);
+              MessageToast.show("Error loading data. Please try again.");
+
+              // debug only
+              this._navigateToDetail(sPurchaseOrder);
+            }.bind(this)
+          );
       },
 
       _navigateToDetail: function (sPurchaseOrder) {
