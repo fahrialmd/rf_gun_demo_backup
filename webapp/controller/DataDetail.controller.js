cb630d5af8472575f58e555a165349c42d98d282
iqbalnurakbar <iqbal.nurakbar25@gmail.com>
2025-07-11 05:52:06 +0700

    change logic for data acquisition

--- webapp/controller/DataDetail.controller.js
+++ webapp/controller/DataDetail.controller.js
@@ -7,6 +7,7 @@ sap.ui.define(
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
   ],
+  
   (Controller, MessageToast, History, JSONModel, Filter, FilterOperator) => {
     'use strict';
 
@@ -15,22 +16,20 @@ sap.ui.define(
         const that = this;
         const oRouter = this.getOwnerComponent().getRouter();
 
-        oRouter.attachRouteMatched(
-          function (oEvent) {
-            const oArgs = oEvent.getParameter('arguments');
-            if (oArgs && oArgs.purchaseOrderNumber) {
-              that.getView().setBusy(true);
-              that
-                ._loadPurchaseOrderData(oArgs.purchaseOrderNumber)
-                .then(() => {
-                  that.getView().setBusy(false);
-                })
-                .catch(() => {
-                  that.getView().setBusy(false);
-                });
-            }
-          }.bind(this)
-        );
+        oRouter.attachRouteMatched(function (oEvent) {
+          const oArgs = oEvent.getParameter('arguments');
+          if (oArgs && oArgs.purchaseOrderNumber) {
+            that.getView().setBusy(true);
+            that
+              ._loadPurchaseOrderData(oArgs.purchaseOrderNumber)
+              .then(() => {
+                that.getView().setBusy(false);
+              })
+              .catch(() => {
+                that.getView().setBusy(false);
+              });
+          }
+        });
 
         this._attachInputEventDelegates();
       },
@@ -47,28 +46,7 @@ sap.ui.define(
         }
       },
 
-      onSaveAndPostButtonPress: function () {
-        const oList = this.byId('orderList');
-        const aSelectedItems = oList.getSelectedItems();
-        const aSelectedData = [];
-
-        aSelectedItems.forEach(item => {
-          const oItemData = {};
-          const oInputs = item.getContent()[0].getItems();
-
-          oItemData['lineNo'] = oInputs[0].getItems()[1].getValue();
-          oItemData['material'] = oInputs[1].getItems()[1].getValue();
-          oItemData['materialDesc'] = oInputs[2].getItems()[1].getValue();
-          oItemData['quantitySuggest'] = oInputs[3].getItems()[1].getValue();
-          oItemData['quantityReceive'] = oInputs[4].getItems()[1].getValue();
-          oItemData['plant'] = oInputs[5].getItems()[1].getValue();
-          oItemData['storageLocation'] = oInputs[6].getItems()[1].getValue();
-
-          aSelectedData.push(oItemData);
-        });
-
-        console.log('Data from Selected Items:', aSelectedData);
-      },
+      onSaveAndPostButtonPress: function () {},
 
       _attachInputEventDelegates: function () {
         const oDataDetailPage = this.byId('dataDetailPage');
@@ -140,10 +118,6 @@ sap.ui.define(
               '/items',
               aContexts.map(oContext => oContext.getObject())
             );
-
-            this._bindDataToList(
-              aContexts.map(oContext => oContext.getObject())
-            );
           }.bind(this),
           function () {
             MessageToast.show(
@@ -151,149 +125,6 @@ sap.ui.define(
             );
           }.bind(this)
         );
-      },
-
-      _bindDataToList: function (aItems) {
-        const oList = this.byId('orderList');
-        oList.bindItems({
-          path: 'purchaseOrder>/items',
-          factory: this._createListItem.bind(this),
-          templateShareable: false,
-        });
-
-        // Focus on the 'Quantity Receive' input for the first item after all items are rendered.
-        oList.attachEventOnce('updateFinished', function () {
-          setTimeout(() => {
-            const oItems = oList.getItems();
-            if (oItems.length > 0) {
-              const oInput = oItems[0]
-                .getContent()[0]
-                .getItems()[4]
-                .getItems()[1];
-              if (oInput) {
-                oInput.focus();
-              }
-            }
-          }, 500);
-        });
-      },
-
-      _createListItem: function (sId, oContext) {
-        return new sap.m.CustomListItem(sId, {
-          content: [
-            new sap.m.HBox({
-              items: [
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Line No',
-                      labelFor: `${sId}-purchaseOrderItemNoInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-purchaseOrderItemNoInput`,
-                      value: '{purchaseOrder>PurchaseOrderItemNo}',
-                      editable: false,
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Material',
-                      labelFor: `${sId}-materialInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-materialInput`,
-                      value: '{purchaseOrder>Material}',
-                      editable: false,
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Material Desc',
-                      labelFor: `${sId}-materialDescriptionInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-materialDescriptionInput`,
-                      value: '{purchaseOrder>MaterialDescription}',
-                      editable: false,
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                ,
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Quantity Suggest',
-                      labelFor: `${sId}-quantitySuggestInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-quantitySuggestInput`,
-                      value: '{purchaseOrder>QuantitySuggest}',
-                      editable: false,
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                ,
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Quantity Receive',
-                      labelFor: `${sId}-quantityReceiveInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-quantityReceiveInput`,
-                      value: '',
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                ,
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Plant',
-                      labelFor: `${sId}-plantInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-plantInput`,
-                      value: '{purchaseOrder>Plant}',
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                ,
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Storage Location',
-                      labelFor: `${sId}-storageLocationInput`,
-                    }),
-                    new sap.m.Input({
-                      id: `${sId}-storageLocationInput`,
-                      value: '{purchaseOrder>StorageLocation}',
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                ,
-                new sap.m.VBox({
-                  items: [
-                    new sap.m.Label({
-                      text: 'Confirm Status',
-                      labelFor: `${sId}-confirmStatusButton`,
-                    }),
-                    new sap.m.Button({
-                      id: `${sId}-confirmStatusButton`,
-                      text: 'OK',
-                      width: '100%',
-                    }),
-                  ],
-                }).addStyleClass('sapUiSmallMargin'),
-                ,
-              ],
-            }),
-          ],
-        });
       },
     });
   }
